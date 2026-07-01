e3574463cb722af153e96f189e925a5e1d4b400d
Wahyu Din <153722974+wahyu14app@users.noreply.github.com>
2026-07-01 10:50:14 +0700

    build: update application ID

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.litert.kjfwnb"
+    applicationId = "com.aistudio.litert.xyzabf"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
